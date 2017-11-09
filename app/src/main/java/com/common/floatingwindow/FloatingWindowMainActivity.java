db92359171400cc8d784a2f1438a711706d4c2d5
popfisher <774232122@qq.com>
2017-11-09 11:17:42 +0800

    新建工程，此工程专门用于整理收集Android知识

---
+++ app/src/main/java/com/common/floatingwindow/FloatingWindowMainActivity.java
@@ -0,0 +1,13 @@
+package com.common.floatingwindow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FloatingWindowMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_window_main);
+    }
+}
